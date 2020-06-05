2b9a6764c87ff835543c45c345aefa5b5387b4e1
Larry <larryosak@gmail.com>
2020-06-04 19:54:41 -0700

    prepare for production

--- server.js
+++ server.js
@@ -10,14 +10,22 @@ connectDB();
 // Init Middleware
 app.use(express.json({extended: false}))
 
-app.get('/', (req, res) => res.send('api running'));
-
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/tournaments', require('./routes/api/tournaments'));
 app.use('/api/contestants', require('./routes/api/contestants'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 const PORT = process.env.PORT || 5500;
 
